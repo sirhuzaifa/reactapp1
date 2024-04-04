783b4cd1a10278ad87d1ee8dbce6e39a59b4ba53
sirhuzaifa <113245558+sirhuzaifa@users.noreply.github.com>
2024-04-04 17:21:39 +0500

    Update Todo.jsx

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Todo() {
   return (
     <>
-        <h1 className='text-lg text-black '>Todo List App</h1>
+        <h1 className='text-lg text-black '>Todo List By Huzaifa</h1>
     </>
   )
 }
