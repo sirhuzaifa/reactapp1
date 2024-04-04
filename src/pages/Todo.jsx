75dce1e85875c241ab560a657883133f7f6b67ae
sirhuzaifa <113245558+sirhuzaifa@users.noreply.github.com>
2024-04-04 17:54:29 +0500

    Update Todo.jsx

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FcTodoList } from "react-icons/fc";
+
 
 function Todo() {
   return (
     <>
-        <h1 className='text-lg text-black '>Todo List By Huzaifa</h1>
+  
+        <h1 className='text-lg text-black flex justify-center font-bold items-center gap-2 font-mono m-10'><FcTodoList size={25} /> Todo List By Huzaifa</h1>
+   
     </>
   )
 }
